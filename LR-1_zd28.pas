827e7847c314610e0ea48994d53cb67e28891b77
ksonend <144596502+ksonend@users.noreply.github.com>
2023-09-21 11:17:45 +0300

    Add files via upload

---
+++ LR-1_zd28.pas
@@ -0,0 +1,16 @@
+﻿program zd;
+var
+  n: int64;
+begin
+  writeln('введите число коров:');
+  read(n);
+  write (' На лугу пасется ', n);
+   if (n mod 10 = 1) 
+   then write  (' корова.')
+  else
+  if  (n mod 10 =2) or (n mod 10 =3) or (n mod 10 =4)  
+    then write(' коровы.')
+  else 
+    write(' коров.');
+  
+end.
